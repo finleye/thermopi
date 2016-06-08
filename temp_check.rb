46781410cf1b53733aa6fa04485d685275cba4b2
Corey Finley <finley.corey@gmail.com>
2016-06-08 16:33:50 -0400

    updates logging

--- temp_check.rb
+++ temp_check.rb
@@ -8,20 +8,25 @@ sensor_serial = `ls /sys/bus/w1/devices/`.split("\n").first
 
 ac_device = Wemote::Switch.find("Air Conditioner")
 
+threshold = 0.5
+target_temp = 75.0
+
 while true
   sensor = TempSesor.new(sensor_serial)
   temp = sensor.temp
 
-  human_temp = temp.round(2)
+  human_temp = temp.round(0)
 
-  if temp > 75 && ac_device.off?
-    puts "Turning Air Conditioner on because it's #{human_temp}"
+  puts "Target Temp: #{target_temp}"
+  puts "Current Temp: #{human_temp}º"
+  puts "****************************"
+
+  if temp > (target_temp + threshold) && ac_device.off?
+    puts "Turning A/C on"
     ac_device.on!
-  elsif temp < 73 && ac_device.on?
-    puts "Turning Air Conditioner off because it's #{human_temp}"
+  elsif temp < (target_temp - threshold) && ac_device.on?
+    puts "Turning A/C off"
     ac_device.off!
-  else
-    puts "It's #{human_temp} so the AC should stay #{ac_device.on? ? "on" : "off"}"
   end
 
   CSV.open("./tmp_log.csv", "ab") do |csv|
